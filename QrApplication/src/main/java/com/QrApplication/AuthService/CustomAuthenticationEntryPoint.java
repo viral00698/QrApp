4b0c64679763ad1480a7f9a9658d9ceac22cdbe6
Viralkumar <viral00698@gmail.com>
2024-08-25 10:50:57 +0530

    code_correction_securrityCustomAuthEntry

--- QrApplication/src/main/java/com/QrApplication/AuthService/CustomAuthenticationEntryPoint.java
+++ QrApplication/src/main/java/com/QrApplication/AuthService/CustomAuthenticationEntryPoint.java
@@ -2,9 +2,15 @@ package com.QrApplication.AuthService;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.QrApplication.AuthSecret.ResponseType;
+import com.QrApplication.Enum.RequestStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,19 +18,31 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CustomAuthenticationEntryPoint  implements AuthenticationEntryPoint {
 
-
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			org.springframework.security.core.AuthenticationException authException)
 			throws IOException, ServletException {
-//			System.err.println(response.getStatus());
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		System.err.println(authentication.getName());
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			response.reset();
-        	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        	response.getWriter().write("Unauthorized: Authentication is required");
-        	response.getWriter().flush();
+//        	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        	response.getWriter().write("Unauthorized: Authentication is required");
+//        	response.getWriter().flush();
+		
+		 	response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+	        response.setCharacterEncoding("UTF-8");
+
+	        // Set the response status code
+	        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+	        // Create a JSON object for the error message
+//	        String jsonResponse = "{ \"error\": \"Unauthorized: Authentication is required\" }";
+	        String jsonResponse= objectMapper.writeValueAsString(new ResponseType<String>().ResponseGenerator(HttpStatus.BAD_REQUEST , RequestStatus.failure, "Unauthorized: Authentication is required"));
+	        // Write the JSON response
+	        response.getWriter().write(jsonResponse);
+	        response.getWriter().flush();
 	}
 
 }
